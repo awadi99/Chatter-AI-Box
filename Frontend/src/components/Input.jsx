57d47d37ded7de5df4dec32f3aa3c1257ea13e71
awadi99 <adityawaghmare9990@gmail.com>
2025-11-06 01:31:09 +0530

    spelling mistake

--- Frontend/src/components/Input.jsx
+++ Frontend/src/components/Input.jsx
@@ -8,7 +8,6 @@ function Input() {
     const handleValue=(event)=>{
         const {name,value}=event.target;
         SetText({ ...text, [name]: value });
-        console.log(text);
     }
 
 
@@ -17,7 +16,7 @@ function Input() {
             <input
                 type="text"
                 className={`text-[20px] w-full px-4 py-2 h-[40px] rounded-2xl border text-sm font-medium
-                text-white" : "border-violet-500 text-violet-400
+                border-violet-500 text-violet-400
                 transition-all  hover:text-white focus:outline-none focus:ring-2 focus:ring-violet-500`}
                 placeholder="Type your message"
                 value={text.message}
