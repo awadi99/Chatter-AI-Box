1db789689dec348c3673fe28388e5181a9bdc1f8
aditya waghmare <149526280+awadi99@users.noreply.github.com>
2025-11-21 21:34:36 +0530

    Update AiChats.jsx

--- Frontend/src/components/AiChats.jsx
+++ Frontend/src/components/AiChats.jsx
@@ -67,7 +67,7 @@ export function AiChats() {
 
     return (
         <>
-            <div className='h-[775px] w-full'>
+            <div className='h-[775px] w-full p-2 '>
                 <div
                     className={` p-3 mt-2 w-full rounded-2xl cursor-pointer text-violet-400
                     transition-all hover:text-purple-500`}
